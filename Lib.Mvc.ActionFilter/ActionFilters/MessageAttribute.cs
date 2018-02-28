e98819dc1200c8b8e94955ca9fa0203feb4c1268
mRRa <spiriturl@gmail.com>
2018-02-28 16:20:30 +0800

    Add MessageAttribute on ActionExecuting

---
+++ Lib.Mvc.ActionFilter/ActionFilters/MessageAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Lib.Mvc.ActionFilter.ActionFilters
+{
+    public class MessageAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            filterContext.Controller.ViewBag.Message = "我來自MessageAttribute";
+        }
+    }
+}
